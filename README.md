74f19a7b9d2f425b97a95ca5b27d3995150929f7
Drakezul <sebastian.grueb@web.de>
2017-05-31 13:07:36 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>IGT Aufgabe2</h1>
+
+Beweisen Sie anhand einer eigenen Beispielimplementierung, dass Hibernate keine geschachtelten Transaktionen zulässt.
---
+++ src/main/java/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package example;/**
+ * Created by ajo on 30.05.17.
+ */
+public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
